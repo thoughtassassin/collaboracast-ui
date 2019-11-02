4e08d85d36a6bbb51476ef7e6e47107d4fcfa17b
David Krause <davidkrause@Davids-MBP.lan1>
2019-11-02 11:36:17 -0500

    Add login messages

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import React, { useState } from "react";
+import Login from "./components/Login/Login";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState({ username: "", password: "" });
-  const handleSubmit = e => {
-    e.preventDefault();
-    fetch("https://collaboracast.herokuapp.com/api/v1/login", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        username: user.username,
-        password: user.password
-      })
-    })
-      .then(response => response.text())
-      .then(token => localStorage.setItem("token", JSON.parse(token).data));
-  };
+  const [authenticated, setAuthenticated] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Don-Nan</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>
-              Username
-              <input
-                name="username"
-                type="text"
-                value={user.username}
-                onChange={e => setUser({ ...user, username: e.target.value })}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Password
-              <input
-                name="username"
-                type="password"
-                value={user.password}
-                onChange={e => setUser({ ...user, password: e.target.value })}
-              />
-            </label>
-          </div>
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
-      </header>
+      {!authenticated ? (
+        <Login setAuthenticated={setAuthenticated} />
+      ) : (
+        <div>logged in</div>
+      )}
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function Login({ setAuthenticated }) {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+  const authenticate = json => {
+    if (json.status === "success") {
+      localStorage.setItem("token", json.data);
+      setAuthenticated(true);
+    } else if (json.status === "error") {
+      setError(json.message);
+    }
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetch("https://collaboracast.herokuapp.com/api/v1/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: user.username,
+        password: user.password
+      })
+    })
+      .then(response => response.json())
+      .then(authenticate);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Don-Nan</h1>
+        {error && <div className="error">{error}</div>}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              Username
+              <input
+                name="username"
+                type="text"
+                value={user.username}
+                onChange={e => setUser({ ...user, username: e.target.value })}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password
+              <input
+                name="username"
+                type="password"
+                value={user.password}
+                onChange={e => setUser({ ...user, password: e.target.value })}
+              />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
