c2fa904adfd4f715282f11280c91c3bcb947ca36
David Krause <davidkrause@Davids-MBP.lan1>
2019-11-03 12:00:24 -0600

    Navigate to Dashboard when selecting feeds

--- src/components/Feeds/Feeds.js
+++ src/components/Feeds/Feeds.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { navigate } from "@reach/router";
 import { Menu } from "semantic-ui-react";
 
 const Feeds = ({ feeds, setFeed, feed }) => (
@@ -9,7 +10,10 @@ const Feeds = ({ feeds, setFeed, feed }) => (
           key={feedItem.id}
           active={feed.toString() === feedItem.id.toString()}
           name={feedItem.name}
-          onClick={() => setFeed(feedItem.id.toString())}
+          onClick={() => {
+            setFeed(feedItem.id.toString());
+            return navigate("/");
+          }}
         />
       ))}
   </Menu>
