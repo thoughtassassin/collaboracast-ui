7b4736d854645c8bfb1558a1a5ff9995bcb3af13
David Krause <davidkrause@Davids-MBP.lan1>
2019-11-29 18:20:50 -0600

    Remove unused dependency

--- src/components/Channels/Channels.js
+++ src/components/Channels/Channels.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { List, Container } from "semantic-ui-react";
+import { List } from "semantic-ui-react";
 
 import ContentSelector from "./ContentSelector/ContentSelector";
 import "./Channels.css";
